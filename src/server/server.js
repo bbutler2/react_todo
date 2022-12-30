a0f2e4b71b54f98f2d348edea937f4a39607d179
bbutler2 <baseballbutler@gmail.com>
2022-12-30 16:31:16 -0500

    fixed server bug. will start implementing mongodb

--- src/server/server.js
+++ src/server/server.js
@@ -1,36 +1,7 @@
 // make sure the dependencies are installed
 const express = require('express');
 const cors = require('cors');
-const {mongoClient, MongoClient} = require('mongodb');
 
-// retrieves a list of databases in the cluster and prints the results
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-// connect to mongodb here
-async function main() {
-    // connection uri (a uniform resource identifier) 
-    const uri = "mongodb+srv://cluster0.i720tg0.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-    try {
-        // connect to the mongodb cluster
-        await client.connect();
-
-        // make the appropriate db calls
-        await listDatabases(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-
-}
-
-main().catch(console.error);
 
 // creating an express application
 const app = express();
