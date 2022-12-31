5228d0e93eb9277f544179140aa0791a0de97bde
bbutler2 <baseballbutler@gmail.com>
2022-12-30 19:36:46 -0500

    now connects to mongodb

--- src/server/server.js
+++ src/server/server.js
@@ -1,26 +1,45 @@
-// make sure the dependencies are installed
-const express = require('express');
-const cors = require('cors');
+// // make sure the dependencies are installed
+// const express = require('express');
+// const cors = require('cors');
 
 
-// creating an express application
+// // creating an express application
+// const app = express();
+// // allows cross-origin requests
+// app.use(cors());
+// // used to parse the incoming JSON payloads
+// app.use(express.json());
+
+// app.post("/post", (req, res) => {
+//     console.log("Connected to React");
+//     res.redirect("/");
+// });
+
+// const PORT = process.env.PORT || 8080;
+
+// // starts the server
+// /*
+//     first argument is the port number
+//     second argument is the callback function
+// */
+// app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+const express = require("express");
 const app = express();
-// allows cross-origin requests
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 app.use(cors());
-// used to parse the incoming JSON payloads
 app.use(express.json());
-
-app.post("/post", (req, res) => {
-    console.log("Connected to React");
-    res.redirect("/");
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./database/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
 });
-
-const PORT = process.env.PORT || 8080;
-
-// starts the server
-/*
-    first argument is the port number
-    second argument is the callback function
-*/
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
